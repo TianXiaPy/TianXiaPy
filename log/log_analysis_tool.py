de17a9f93d14589a5f64173318b7ea82f050c79f
TianxiaPy <17600299187@163.com>
2020-03-21 22:21:45 +0800

    add str method:endswith()

--- log/log_analysis_tool.py
+++ log/log_analysis_tool.py
@@ -2,8 +2,16 @@
 
 import os
 import sys
-import os.path as ospath
+import os.path as os_path
 
+
+class LogFile(object):
+
+    def __init__(self):
+        self.log = ".log"
+
+    def endswith(self, type):
+        pass
 
 
 if __name__ == "__main__":
